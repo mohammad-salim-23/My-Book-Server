ec5930654f7e6627b4800ccae8d8ab06f95c2fdc
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-06-30 12:15:29 -0700

    verifyToken and verifyAdmin middleware create

--- index.js
+++ index.js
@@ -28,7 +28,45 @@ async function run() {
     const bookCollection = client.db("bookDB").collection("books");
 
     // middleWare
-    
+    const verifyToken = (req,res,next)=>{
+        console.log("inside verify token",req.headers);
+        if(!req.headers.authorization){
+            return res.status(401).send(
+                {message:"unauthorized access"}
+            )
+        }
+        const token = req.headers.authorization.split('')[1];
+        jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+            if(err){
+                return res.status(401).send({
+                    message:"unauthorized access"
+                })
+            }
+            req.decoded = decoded;
+            next();
+        })
+    }
+    // use verify admin after 
+    const verifyAdmin = async(req,res,next)=>{
+        const email = req.decoded.email;
+        const query = {email:email};
+        const user = await userCollection.findOne(query);
+        const isAdmin = user?.role==='admin';
+        if(!isAdmin){
+          return res.status(403).send({message:'forbidden access true'});
+        }
+        next();
+       }
+    //    jwt related api
+    app.post("/jwt",async(req,res)=>{
+        const user = req.body;
+        console.log("jwt...",user);
+        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:'1h'
+        });
+        res.send({token});
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
