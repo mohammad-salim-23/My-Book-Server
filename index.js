1f9e8b9070cd333a4d41d815e43c4585e53e868b
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-07-15 02:05:21 +0600

    delete api create for bookCollection

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(helmet({
   crossOriginOpenerPolicy: { policy: "same-origin-allow-popups" }
 }));
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipsrkdy.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -102,13 +102,18 @@ async function run() {
       const result = await bookCollection.insertOne(book);
       res.send(result);
     })
-    app.get("books/:email",verifyToken,async(req,res)=>{
+    app.get("/books/:email",verifyToken,async(req,res)=>{
       const email = req.params.email;
       const query = {email:email};
       const result = await bookCollection.find(query).toArray();
       res.send(result);
     })
-
+   app.delete("/books/:id",verifyToken,async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id:new ObjectId(id)};
+    const result = await bookCollection.deleteOne(query);
+    res.send(result);
+   })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
